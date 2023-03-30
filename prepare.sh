b19c7c77fe7c3c78e4a3f5ce8779193a14694cb4
qingwei4 <tery3693tery@gmail.com>
2023-03-31 03:04:17 +0800

    change python-dev in prepare.sh to python3-dev

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential python-dev python3-pip libffi-dev
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential python3-dev python3-pip libffi-dev
 pip install -r requirements.txt
