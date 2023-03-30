3621552db87a648fdc8b831c66dc7e83640d5e92
qingwei4 <tery3693tery@gmail.com>
2023-03-31 03:37:19 +0800

    add python3-testresources to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sudo apt-get update
+sudo apt-get install -y python3-testresources
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential python3-dev python3-pip libffi-dev
 pip install -r requirements.txt
